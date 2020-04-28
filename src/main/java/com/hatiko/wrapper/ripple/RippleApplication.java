399e563fd19e78cb54b22625f5bf6c4ddde56fc0
Hat1ko <pasha1apple@gmail.com>
2020-04-28 17:36:39 +0300

    created basic package and application.properties structure

---
+++ src/main/java/com/hatiko/wrapper/ripple/RippleApplication.java
@@ -0,0 +1,11 @@
+package com.hatiko.wrapper.ripple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RippleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RippleApplication.class, args);
+    }
+}
