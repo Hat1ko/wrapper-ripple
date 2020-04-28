4800c1d8c577de0f26327f809405198bfc5bd866
Hat1ko <pasha1apple@gmail.com>
2020-04-28 18:13:27 +0300

    created BlockchainProperties.class

---
+++ src/main/java/com/hatiko/wrapper/ripple/properties/BlockchainProperties.java
@@ -0,0 +1,36 @@
+package com.hatiko.wrapper.ripple.properties;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ConfigurationProperties("blockchain")
+@Component
+public final class BlockchainProperties {
+    String uri;
+    Method method;
+
+    @Data
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    public static final class Method {
+
+        String accountChannels;
+        String accountCurrencies;
+        String accountInfo;
+        String accountLines;
+        String accountObjects;
+        String accountTransactions;
+        String gatewayBalances;
+
+        String sign;
+        String signFor;
+        String submit;
+
+        String transaction;
+    }
+
+}
